94dbd2da4e025777beccc6ce89aa388b6a9292e3
netto.cavalcanti <netto.cavalcanti@gmail.com>
2021-11-21 09:39:47 -0300

    Changing link to button in home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,15 @@
+import { Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import { ThumbUp } from "@material-ui/icons";
+
 const Home = () => {
     return (
         <div>
             <h1>Custom Form Builder</h1>
-            <h5>Welcome to our best web application for building custom Forms!</h5>
+            <h5>Welcome to the best web application for building custom Forms!</h5>
 
             <h6>Start today your own form clicking the link below!</h6>
-            <a href="/forms">Show me what you got!</a>
+            <Button variant="contained" color="secondary" component={Link} startIcon={<ThumbUp />} to="/forms">Show me what you got!</Button>
         </div>
     );
 }
